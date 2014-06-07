27586a095f3f9a295416e50698fe480958b0a765
YaoChang <yaochang@ubuntu.(none)>
2014-06-07 14:57:28 +0800

    remove duplicates from sorted lists

---
+++ LeetCode/remove duplicates from sorted list/main.c
@@ -0,0 +1,22 @@
+tion for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        if(head==NULL) return NULL;
+        ListNode *h = head;
+        while(head->next){
+            if(head->val==head->next->val){
+                head->next = head->next->next;
+            }else{
+                head=head->next;
+            }
+        }
+        return h;
+    }
+};
